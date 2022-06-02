f296236d68c09c72f7090241ac292f8aa20166af
Duval Kilpatrick <mkilpatrick@yext.com>
2022-06-02 11:45:50 -0400

    Make liveApi query on window open

--- popup.js
+++ popup.js
@@ -4,23 +4,23 @@ chrome.storage.sync.get("liveApiKey", ({ liveApiKey }) => {
   liveApiKeyDiv.innerHTML = "Using liveApiKey: " + liveApiKey;
 });
 
-// When the button is clicked, inject setPageBackgroundColor into current page
-liveApiKeyDiv.addEventListener("click", async () => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: queryLiveApi,
-  });
-});
+window.onload = function() {
+  queryLiveApi();
+}
 
 /**
  * Queries LiveApi to get the data of an entity based on the current url.
  */
-function queryLiveApi() {
-  chrome.storage.sync.get("liveApiKey", ({ liveApiKey }) => {
+const queryLiveApi = () => {
+  chrome.storage.sync.get("liveApiKey", async({ liveApiKey }) => {
     const path = window.location.pathname;
+    const queryUrl = `https://liveapi.yext.com/v2/accounts/me/entities/1985?api_key=${liveApiKey}&v=20220602`;
+    console.log(queryUrl);
 
-
+    await fetch(queryUrl)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+      });
   });
 }

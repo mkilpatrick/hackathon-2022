b39c2da7b8322412d85e6f2777894afc8e2caf29
Duval Kilpatrick <mkilpatrick@yext.com>
2022-06-02 12:26:31 -0400

    Update liveApi query

--- popup.js
+++ popup.js
@@ -8,19 +8,35 @@ window.onload = function() {
   queryLiveApi();
 }
 
+function getStorageData(key) {
+  return new Promise((resolve) => {
+    chrome.storage.sync.get(key, resolve);
+  });
+}
+
+function getActiveTab() {
+  return new Promise((resolve) => {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      resolve(tabs[0]);
+    });
+  });
+}
+
 /**
  * Queries LiveApi to get the data of an entity based on the current url.
  */
-const queryLiveApi = () => {
-  chrome.storage.sync.get("liveApiKey", async({ liveApiKey }) => {
-    const path = window.location.pathname;
-    const queryUrl = `https://liveapi.yext.com/v2/accounts/me/entities/1985?api_key=${liveApiKey}&v=20220602`;
-    console.log(queryUrl);
+const queryLiveApi = async() => {
+  const { liveApiKey } =  await getStorageData("liveApiKey");
+  let url = (await getActiveTab()).url;
 
-    await fetch(queryUrl)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-      });
+  const filter = encodeURIComponent(`"{websiteUrl.url": {"$eq": "${url}"}}`);
+  const queryUrl = `https://liveapi.yext.com/v2/accounts/me/entities?api_key=${liveApiKey}&v=20220602&filter=${filter}`;
+  // https://liveapi.yext.com/v2/accounts/me/entities?api_key={apikey}&v=20220602&filter={url_encoded_filter_json}
+  console.log(queryUrl);
+
+  await fetch(queryUrl)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
   });
 }

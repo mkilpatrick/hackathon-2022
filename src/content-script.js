d4ea565b800706fc0a7b9c5e8de20562fb29b3de
nmanu1 <nmanu@yext.com>
2022-06-02 16:33:10 -0400

    Add style

--- src/content-script.js
+++ src/content-script.js
@@ -2,11 +2,38 @@ const findAndReplaceDOMText = require('findAndReplaceDOMText');
 
 // Listen for messages
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
+  const style = document.createElement('style');
+  
+  style.textContent = `
+    .yextfield {
+      position: relative;
+      display: inline-block;
+      border-bottom: 1px dotted black; /* If you want dots under the hoverable text */
+    }
+    .yextfield .tooltipText {
+      visibility: hidden;
+      width: 120px;
+      background-color: black;
+      color: #fff;
+      text-align: center;
+      padding: 5px 0;
+      border-radius: 6px;
+
+      position: absolute;
+      z-index: 1;
+    }
+    .yextfield:hover .tooltipText {
+      visibility: visible;
+    }
+  `;
+  document.head.appendChild(style);
+
   if (msg.entity) {
     Object.entries(msg.entity).forEach(([key, val]) => {
       let node = document.createElement('span');
       node.style.outlineStyle = 'solid';
       node.style.position = 'relative';
+      node.className = 'tooltip';
 
       let tooltipNode = document.createElement('span');
       tooltipNode.innerText = val;
@@ -21,6 +48,7 @@ chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
       tooltipNode.style.fontSize = '12px';
       tooltipNode.style.fontWeight = 'bold';
       tooltipNode.style.padding = '2px 3px';
+      tooltipNode.className = 'tooltipText'
 
       node.appendChild(tooltipNode);
 
@@ -28,7 +56,9 @@ chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
         find: key,
         wrap: node,
         wrapClass: 'yextfield'
-      });      
+      }); 
+      
     });
   }
+  sendResponse('done');
 });

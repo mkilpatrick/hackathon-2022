105e18fb593a4c303a91878a7ebd14cad65060f9
Duval Kilpatrick <mkilpatrick@yext.com>
2022-06-02 16:15:56 -0400

    Show tags on page

--- src/content-script.js
+++ src/content-script.js
@@ -3,18 +3,32 @@ const findAndReplaceDOMText = require('findAndReplaceDOMText');
 // Listen for messages
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
   if (msg.entity) {
-      console.log('msg.entity', msg.entity);
+    Object.entries(msg.entity).forEach(([key, val]) => {
+      let node = document.createElement('span');
+      node.style.outlineStyle = 'solid';
+      node.style.position = 'relative';
 
-      //mock data for when we figure out the string matching process
-      const testData = [{
-        field: 'c_someField',
-        value: 'some random text'
-      }]
-      sendResponse(testData);
+      let tooltipNode = document.createElement('span');
+      tooltipNode.innerText = val;
+      tooltipNode.style.position = 'absolute';
+      tooltipNode.style.zIndex = '1';
+      tooltipNode.style.top = '-20px';
+      tooltipNode.style.left = '-20px';
+      tooltipNode.style.backgroundColor = 'black';
+      tooltipNode.style.color = 'white';
+      tooltipNode.style.width = 'auto';
+      tooltipNode.style.height = 'auto';
+      tooltipNode.style.fontSize = '12px';
+      tooltipNode.style.fontWeight = 'bold';
+      tooltipNode.style.padding = '2px 3px';
+
+      node.appendChild(tooltipNode);
+
+      findAndReplaceDOMText(document.body, {
+        find: key,
+        wrap: node,
+        wrapClass: 'yextfield'
+      });      
+    });
   }
-});
-
-findAndReplaceDOMText(document.body, {
-    find: "Daniel",
-    wrap: 'em'
 });

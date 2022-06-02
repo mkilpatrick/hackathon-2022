c94b2cf542d888a5107d1e7b2e589e9f4fc516f9
nmanu1 <nmanu@yext.com>
2022-06-02 16:34:53 -0400

    Clean up styling

--- src/content-script.js
+++ src/content-script.js
@@ -31,18 +31,12 @@ chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
   if (msg.entity) {
     Object.entries(msg.entity).forEach(([key, val]) => {
       let node = document.createElement('span');
-      node.style.outlineStyle = 'solid';
-      node.style.position = 'relative';
       node.className = 'tooltip';
 
       let tooltipNode = document.createElement('span');
       tooltipNode.innerText = val;
-      tooltipNode.style.position = 'absolute';
-      tooltipNode.style.zIndex = '1';
       tooltipNode.style.top = '-20px';
       tooltipNode.style.left = '-20px';
-      tooltipNode.style.backgroundColor = 'black';
-      tooltipNode.style.color = 'white';
       tooltipNode.style.width = 'auto';
       tooltipNode.style.height = 'auto';
       tooltipNode.style.fontSize = '12px';

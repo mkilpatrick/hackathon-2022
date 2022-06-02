70785eaa1d9e08708be99861280a7cc54fd912fd
John <jokim@yext.com>
2022-06-02 14:30:45 -0400

    Add entity parser

--- popup.js
+++ popup.js
@@ -22,6 +22,32 @@ function getActiveTab() {
   });
 }
 
+function parseResponse(response) {
+  const mappings = {}
+
+  // ignore non-string values
+  function getFieldsHelper (jsonObject, jsonPath, mapping) {
+    for (var key in jsonObject) {
+      const value = jsonObject[key]
+      if (value instanceof Array) {
+        // console.log("I am array")
+        // for (var item in value) {
+        //  mapping[item] = jsonPath + "/" + key
+        // }
+      } else if (typeof value === 'string' || value instanceof String) {
+        mapping[value] = (jsonPath + "." + key).substring(1)
+      } else if (value instanceof Object) {
+        getFieldsHelper(value, jsonPath + "." + key, mapping)
+      }
+    }
+  }
+
+  const mapping = {}
+  getFieldsHelper(entities, "", mapping)
+
+  return mapping
+}
+
 /**
  * Queries LiveApi to get the data of an entity based on the current url.
  */
@@ -38,6 +64,7 @@ const queryLiveApi = async() => {
     .then(data => {
       if (data?.response?.count == 1) {
         entityData = JSON.stringify(data.response.entities[0]);
+        console.log(parseResponse(entityData));
         document.getElementById("apiResponse").innerHTML = entityData;
         chrome.tabs.sendMessage(activeTab.id, { entity: entityData }, function(response) {
           console.log(response);
@@ -47,3 +74,48 @@ const queryLiveApi = async() => {
       }
   });
 }
+
+
+
+class entityparser {
+  constructor(response) {
+    this.response = response
+  }
+
+  getFields = () => {
+    const mappings = {}
+
+    // assume for now that there is only one matching entity
+    const entities = this.response.response.entities[0]
+
+    // ignore non-string values
+    function getFieldsHelper (jsonObject, jsonPath, mapping) {
+      for (var key in jsonObject) {
+        const value = jsonObject[key]
+        if (value instanceof Array) {
+          // console.log("I am array")
+          // for (var item in value) {
+          //  mapping[item] = jsonPath + "/" + key
+          // }
+        } else if (typeof value === 'string' || value instanceof String) {
+          mapping[value] = (jsonPath + "." + key).substring(1)
+        } else if (value instanceof Object) {
+          getFieldsHelper(value, jsonPath + "." + key, mapping)
+        }
+      }
+    }
+
+    const mapping = {}
+    getFieldsHelper(entities, "", mapping)
+
+    return mapping
+  }
+}
+
+fetch('https://liveapi.yext.com/v2/accounts/me/entities?api_key=3edb77b89b2361b2bc0b4cb6c79ab134&v=20220602&filter=%7B%22websiteUrl.url%22%3A%20%7B%22%24eq%22%3A%20%22http%3A%2F%2Fbroomhallbrothers.com%22%7D%7D')
+  .then(response => response.json())
+  .then(data => {
+    const temp = new entityparser(data)
+    console.log(temp.getFields())
+});
+
